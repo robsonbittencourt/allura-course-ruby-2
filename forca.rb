1601de2608e5a6769af4cfda79a52525db74f1e9
robsonbittencourt <robson.luizv@gmail.com>
2021-03-23 21:49:27 -0300

    Extract rank functions to another file

--- forca.rb
+++ forca.rb
@@ -1,14 +1,5 @@
 require_relative 'ui'
-
-def salva_rank(nome, pontos_totais)
-    conteudo = "#{nome}\n#{pontos_totais}"
-    File.write "rank.txt", conteudo
-end
-
-def le_rank
-    conteudo = File.read "rank.txt"
-    conteudo.split "\n"
-end
+require_relative 'rank'
 
 def escolhe_paravra_secreta
     avisa_escolhendo_palavra
